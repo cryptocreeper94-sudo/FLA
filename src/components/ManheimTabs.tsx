9e6e7d61248c9b67cb2f1544dd4a8c3fd87852e3
cryptocreeper94 <jason@livfitoken.com>
2026-05-30 01:20:26 -0500

    fix: remove unused Activity import

--- src/components/ManheimTabs.tsx
+++ src/components/ManheimTabs.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import { Shield, Activity, Cpu } from 'lucide-react';
+import { Shield, Cpu } from 'lucide-react';
 
 export default function ManheimTabs() {
   const { pathname } = useLocation();
